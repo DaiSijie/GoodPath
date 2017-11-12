1a13ebd54553408734a4e82856a09f21989acca7
Gilbert Maystre <gilbert@maystre.ch>
2017-11-12 01:26:21 +0100

    Cleanup of settings activity

--- frontend/app/src/main/java/com/goodpaths/goodpaths/SettingsActivity.java
+++ frontend/app/src/main/java/com/goodpaths/goodpaths/SettingsActivity.java
@@ -4,42 +4,48 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.RadioGroup;
 
+import static com.goodpaths.goodpaths.DangerTypeHelper.ACCESSIBILITY;
+import static com.goodpaths.goodpaths.DangerTypeHelper.HARASSMENT;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    private ImageButton backButton;
+
+    private RadioGroup radioGroupDangerType;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        setSupportActionBar((Toolbar) findViewById(R.id.settingsToolbar));
+        findViews();
+        setViews();
+    }
 
-        Toolbar myBar = findViewById(R.id.my_toolbar);
-        setSupportActionBar(myBar);
+    private void findViews(){
+        backButton = findViewById(R.id.back);
+        radioGroupDangerType = findViewById(R.id.typeGroup);
+    }
 
-        ImageButton back = findViewById(R.id.back);
-
-        ImageButton help = findViewById(R.id.help);
-
-        back.setOnClickListener(new View.OnClickListener() {
+    private void setViews(){
+        backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 onBackPressed();
             }
         });
 
-
-        help.setOnClickListener(new View.OnClickListener() {
+        int currentDangerInt = DangerTypeHelper.getInstance().getDangerType(this);
+        radioGroupDangerType.check(currentDangerInt == ACCESSIBILITY? R.id.radioAccessibility : R.id.radioHarassment);
+        radioGroupDangerType.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onClick(View view) {
-                InfoOverlay.displayOverlay(SettingsActivity.this);
+            public void onCheckedChanged(RadioGroup radioGroup, int id) {
+                int newDangerInt = id == R.id.radioAccessibility ? ACCESSIBILITY : HARASSMENT;
+                DangerTypeHelper.getInstance().setDangerType(SettingsActivity.this, newDangerInt);
             }
         });
-
     }
-
-
-
 }
