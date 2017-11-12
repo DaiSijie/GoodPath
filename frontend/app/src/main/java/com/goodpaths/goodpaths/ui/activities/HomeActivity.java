18a7c436d413a394c38107e3f13dbe653fe59441
Gilbert Maystre <gilbert@maystre.ch>
2017-11-12 07:12:06 +0100

    Implemented go-bar

--- frontend/app/src/main/java/com/goodpaths/goodpaths/ui/activities/HomeActivity.java
+++ frontend/app/src/main/java/com/goodpaths/goodpaths/ui/activities/HomeActivity.java
@@ -8,17 +8,23 @@ import android.os.Vibrator;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.goodpaths.common.Report;
 import com.goodpaths.goodpaths.business.CustomUrlTileProvider;
 import com.goodpaths.goodpaths.business.DangerPointPoster;
 import com.goodpaths.goodpaths.R;
+import com.goodpaths.goodpaths.business.DangerTypeHelper;
 import com.goodpaths.goodpaths.business.LocationProvider;
 import com.goodpaths.goodpaths.ui.HomeActivityImageHelper;
 import com.goodpaths.goodpaths.ui.InfoOverlay;
@@ -30,12 +36,17 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.JointType;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.maps.model.TileOverlay;
 import com.google.android.gms.maps.model.TileOverlayOptions;
 import com.google.android.gms.maps.model.TileProvider;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity implements OnMapReadyCallback, LocationProvider.OnNewLocationReceivedListener, DangerPointPoster.OnMegaSuccessListener {
 
@@ -61,7 +72,12 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
     private ImageButton settingsButton;
     private ImageButton helpButton;
     private FloatingActionButton localizeMeButton;
+    private FloatingActionButton goFAB;
     private FloatingActionButton recenterButton;
+    private SupportMapFragment mapFragment;
+    private EditText destinationText;
+    private Button goButton;
+    private LinearLayout goBaseLayout;
 
     // endregion
 
@@ -77,9 +93,10 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
         dangerPointPoster.setOnMegaSuccessListener(this);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         checkLocationPermission();
+        Fragment f = (Fragment) mapFragment;
 
         if (GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this) != ConnectionResult.SUCCESS) {
             Toast.makeText(this, "Services not available.", Toast.LENGTH_LONG).show();
@@ -93,6 +110,7 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
                     && grantResults[0] == PackageManager.PERMISSION_GRANTED;
             localizeMeButton.setEnabled(enabled);
             recenterButton.setEnabled(enabled);
+            goFAB.setEnabled(enabled);
             return;
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -101,8 +119,10 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap map) {
         this.map = map;
-        TileProvider tp = new CustomUrlTileProvider(512, 512);
+        TileProvider tp = new CustomUrlTileProvider(512, 512, this);
         tileOverlay = this.map.addTileOverlay(new TileOverlayOptions().tileProvider(tp).transparency(0.5f).fadeIn(true));
+        Polyline x = map.addPolyline(new PolylineOptions().addAll(getMockPath()));
+        x.setColor(0xff8fabd5);
     }
 
     @Override
@@ -127,6 +147,14 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     // region helpers
 
+    private ArrayList<LatLng> getMockPath(){
+        ArrayList<LatLng> toReturn = new ArrayList<>();
+        toReturn.add(new LatLng(46.5196535,6.632273400000031));
+        toReturn.add(new LatLng(46.51998896696903,6.625528335571289));
+        toReturn.add(new LatLng(46.52246943970911, 6.61402702331543));
+        return toReturn;
+    }
+
     private void refresh(){
         if(tileOverlay != null){
             tileOverlay.clearTileCache();
@@ -147,6 +175,7 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
         } else {
             localizeMeButton.setEnabled(true);
             recenterButton.setEnabled(true);
+            goFAB.setEnabled(true);
         }
     }
 
@@ -156,6 +185,10 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
         helpButton = findViewById(R.id.help);
         localizeMeButton = findViewById(R.id.button);
         recenterButton = findViewById(R.id.recenterButton);
+        destinationText = findViewById(R.id.destinationText);
+        goButton = findViewById(R.id.goButton);
+        goFAB = findViewById(R.id.goFAB);
+        goBaseLayout = findViewById(R.id.goBaseLayout);
     }
 
     private void initViews(){
@@ -177,7 +210,8 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
         localizeMeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                dangerPointPoster.postCurrentPosition(Report.Type.ACCESSIBILITY);
+                Report.Type toUse = DangerTypeHelper.getInstance().getDangerType(null) == DangerTypeHelper.ACCESSIBILITY ? Report.Type.ACCESSIBILITY : Report.Type.HARASSMENT;
+                dangerPointPoster.postCurrentPosition(toUse);
             }
         });
 
@@ -201,6 +235,22 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
             @Override
             public void onClick(View view) {
                 centerOn(LocationProvider.getInstanceOf(HomeActivity.this).getLocation());
+            }
+        });
+
+        goFAB.setEnabled(false);
+        goFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int toPut = goBaseLayout.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE;
+                goBaseLayout.setVisibility(toPut);
+            }
+        });
+
+        goButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
             }
         });
     }
